77727890955cffb007c477d39f93913d046d611e
ElRiyo <gabrielvrio@alu.ufc.br>
2020-11-27 23:33:07 +0000

    código inserido, testes automáticos ok

--- main.py
+++ main.py
@@ -4,7 +4,12 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
-
+    capital = int(input('Digite o valor principal '))
+    juros = float(input('Digite o valor da taxa de  juros (por mes)'))
+    time = int(input('Digite a quantidade de meses do financiamento '))
+    montante = capital * (1 + juros/100) **time
+    final = montante - capital 
+    print('O valor dos juros é igual à {:.2f}.'.format(final))
 
 if __name__ == '__main__':
     main()
